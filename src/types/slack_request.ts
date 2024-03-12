a0257000d81894ddbbbbddb0f9b3ca67d80d07b3
Anh Mac <git@macanh.com>
2024-03-12 14:24:56 +0700

    fix: correct percent format

--- src/types/slack_request.ts
+++ src/types/slack_request.ts
@@ -22,19 +22,19 @@ export const getSlackMessage = async (request: Request): Promise<SlackMessage> =
   const params = qs.parse(body);
 
   return {
-    token: (params['token'] as string).trim(),
-    teamId: (params['team_id'] as string).trim(),
-    teamDomain: (params['team_domain'] as string).trim(),
-    enterpriseId: (params['enterprise_id'] as string).trim(),
-    enterpriseName: (params['enterprise_name'] as string).trim(),
-    channelId: (params['channel_id'] as string).trim(),
-    channelName: (params['channel_name'] as string).trim(),
-    userId: (params['user_id'] as string).trim(),
-    userName: (params['user_name'] as string).trim(),
-    command: (params['command'] as string).trim(),
-    text: (params['text'] as string).trim(),
-    responseUrl: (params['response_url'] as string).trim(),
-    triggerId: (params['trigger_id'] as string).trim(),
-    apiAppId: (params['api_app_id'] as string).trim(),
+    token: (params['token'] as string ?? "").trim(),
+    teamId: (params['team_id'] as string ?? "").trim(),
+    teamDomain: (params['team_domain'] as string ?? "").trim(),
+    enterpriseId: (params['enterprise_id'] as string ?? "").trim(),
+    enterpriseName: (params['enterprise_name'] as string ?? "").trim(),
+    channelId: (params['channel_id'] as string ?? "").trim(),
+    channelName: (params['channel_name'] as string ?? "").trim(),
+    userId: (params['user_id'] as string ?? "").trim(),
+    userName: (params['user_name'] as string ?? "").trim(),
+    command: (params['command'] as string ?? "").trim(),
+    text: (params['text'] as string ?? "").trim(),
+    responseUrl: (params['response_url'] as string ?? "").trim(),
+    triggerId: (params['trigger_id'] as string ?? "").trim(),
+    apiAppId: (params['api_app_id'] as string ?? "").trim(),
   }
 }
